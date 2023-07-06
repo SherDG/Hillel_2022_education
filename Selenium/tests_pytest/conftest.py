75d485e365e1154ef901d18cbb659c380a96c309
Dmytro Sherstiuk <sherdg@gmail.com>
2023-07-06 20:41:13 +0300

    Fix pytest

--- Selenium/tests_pytest/conftest.py
+++ Selenium/tests_pytest/conftest.py
@@ -4,7 +4,7 @@ import pytest
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 options = Options()
 # options.add_argument("start-maximized"); # open Browser in maximized mode
